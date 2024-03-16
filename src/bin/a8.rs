a629c272340fa8f343e8f60dcf787ee801c1faf2
김혜송(Hyesong Kim)/커머스플랫폼개발팀/SKP <hyesong.kim@sk.com>
2024-03-16 21:28:12 +0900

    solve a8 :: struct

--- src/bin/a8.rs
+++ src/bin/a8.rs
@@ -9,4 +9,46 @@
 // * Use a function to print out the drink flavor and ounces
 // * Use a match expression to print the drink flavor
 
-fn main() {}
+enum Flavor {
+    Chocolate,
+    Vanilla,
+    Mix
+}
+
+struct Drink {
+    flavor: Flavor,
+    fluid: f64
+}
+
+fn print_flavor(flavor:Flavor) {
+    match flavor {
+        Flavor::Chocolate => println!("chocolate"),
+        Flavor::Vanilla => println!("vanilla"),
+        Flavor::Mix => println!("mix")
+    }
+}
+
+fn print_drink(drink:Drink) {
+    print_flavor(drink.flavor);
+    println!("{:?}", drink.fluid);
+}
+
+fn main() {
+    let chocolate = Drink {
+        flavor: Flavor::Chocolate,
+        fluid: 6.0
+    };
+    print_drink(chocolate);
+
+    let vanilla = Drink {
+        flavor: Flavor::Vanilla,
+        fluid: 10.0
+    };
+    print_drink(vanilla);
+
+    let mix = Drink {
+        flavor: Flavor::Mix,
+        fluid: 12.0
+    };
+    print_drink(mix);
+}
