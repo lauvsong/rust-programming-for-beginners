ee3010c4e2162a4ffd92de9fcc0d10a9d5b40e7a
김혜송(Hyesong Kim)/커머스플랫폼개발팀/SKP <hyesong.kim@sk.com>
2024-03-21 00:59:16 +0900

    solve a16 :: option

--- src/bin/a16.rs
+++ src/bin/a16.rs
@@ -8,7 +8,33 @@
 // * Use a struct containing the student's name and locker assignment
 // * The locker assignment should use an Option<i32>
 
-// * Use a struct containing the student's name and locker assignment
-// * The locker assignment should use an Option<i32>
+struct Student {
+    name: String,
+    locker: Option<i32>
+}
 
-fn main() {}
+fn main() {
+
+    let students = vec![
+        Student {
+            name: "Mary".to_owned(),
+            locker: Some(1)
+        },
+        Student {
+            name: "Alice".to_owned(),
+            locker: None
+        },
+        Student {
+            name: "Bob".to_owned(),
+            locker: Some(3)
+        }
+    ];
+
+    for student in &students {
+        println!("number: {:?}", student.name);
+        match student.locker {
+            Some(num) => println!("locker: {:?}", num),
+            None => println!("no locker assigned")
+        }
+    }
+}
