792d58b9d73c7f7fe2e290a8a11f7e908445cf10
김혜송(Hyesong Kim)/커머스플랫폼개발팀/SKP <hyesong.kim@sk.com>
2024-03-24 14:04:06 +0900

    solve a18b :: Result with question mark

--- src/bin/a18b.rs
+++ src/bin/a18b.rs
@@ -22,4 +22,49 @@
 // * Print whether the employee may access the building
 //   * Must use a function that utilizes the question mark operator to do this
 
-fn main() {}
+enum EmployeeType {
+    Maintenance,
+    Marketing,
+    Manager,
+    LineSupervisor,
+    KitchenStaff,
+    AssemblyTechnician
+}
+
+struct KeyCard {
+    employee_type: EmployeeType,
+    employed: bool
+}
+
+fn access_building(key_card: &KeyCard) -> Result<(), String> {
+    match key_card.employed {
+        false => return Err("access denied :: terminated employee".to_owned()),
+        _ => ()
+    }
+
+
+    match key_card.employee_type {
+        EmployeeType::Maintenance => Ok(()),
+        EmployeeType::Manager => Ok(()),
+        EmployeeType::Marketing => Ok(()),
+        _ => Err("access denied :: not granted employee type".to_owned())
+    }
+}
+
+fn print_access(key_card: &KeyCard) -> Result<(), String> {
+    let result = access_building(key_card)?;
+    println!("access ok");
+    Ok(())
+}
+
+fn main() {
+    let key_card = KeyCard {
+        employee_type: EmployeeType::Maintenance,
+        employed: true
+    };
+
+    match print_access(&key_card) {
+        Err(message) => println!("error :: {:?}", message),
+        _ => ()
+    }
+}
