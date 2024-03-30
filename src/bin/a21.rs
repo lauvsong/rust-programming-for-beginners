27476a6abbbff368d41fa87bcdcdb2fa9b32eaf6
김혜송(Hyesong Kim)/커머스플랫폼개발팀/SKP <hyesong.kim@sk.com>
2024-03-30 16:04:00 +0900

    solve a21 :: map combinator

--- src/bin/a21.rs
+++ src/bin/a21.rs
@@ -25,4 +25,16 @@ fn find_user(name: &str) -> Option<i32> {
     }
 }
 
-fn main() {}
+fn main() {
+    let user_name = "sam";
+    let user = find_user(user_name)
+        .map(|user_id| User {
+            user_id,
+            name: user_name.to_owned()
+        });
+
+    match user {
+        Some(user) => println!("user: {:?}", user),
+        None => println!("nt found")
+    }
+}
